d8cccb240498ff878910394c788b79bc9c82d6e3
Kevin Gallardo <andreskevin2606@gmail.com>
2025-02-24 21:48:20 -0500

    feat: actualizar configuración para usar Bun y Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
-import solidJs from '@astrojs/solid-js';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
